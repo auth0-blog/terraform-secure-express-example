8748ec27c9dd7d17bf6d674b85154ce6f04f68bf
Mario Fernandez <mario.fernandez@hceris.com>
2021-07-07 22:26:35 +0200

    add providers

--- main.tf
+++ main.tf
@@ -21,6 +21,20 @@ variable "auth0_domain" {}
 variable "auth0_client_id" {}
 variable "auth0_client_secret" {}
 
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.13.0"
+    }
+
+    auth0 = {
+      source  = "alexkappa/auth0"
+      version = "~> 0.16.0"
+    }
+  }
+}
+
 provider "auth0" {
   domain        = var.auth0_domain
   client_id     = var.auth0_client_id
